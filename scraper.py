fecdd827805e4920e511d91f01e80fa8ed13d1f3
Ivan Leung <ivanl8866@gmail.com>
2024-10-21 21:53:21 -0700

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,69 @@
+import re
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+def scraper(url, resp):
+    links = extract_next_links(url, resp)
+    return [link for link in links if is_valid(link)]
+
+def extract_next_links(url, resp):
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+
+    # print(f"Extracting links from {url}")
+    # soup = BeautifulSoup(resp.raw_response.content, "lxml")
+    links = extract_links(url, resp)
+    # print(f"{len(links)} links extracted from {url}")
+    # print(links)
+    return [link for link in links if is_valid(link)]
+
+def is_valid(url):
+    # Decide whether to crawl this url or not. 
+    # If you decide to crawl it, return True; otherwise return False.
+    # There are already some conditions that return False.
+    allowed_domains = [
+        "ics.uci.edu",
+        "cs.uci.edu",
+        "informatics.uci.edu",
+        "stat.uci.edu",
+        "today.uci.edu"
+    ]
+
+    try:
+        parsed = urlparse(url)
+        if parsed.scheme not in set(["http", "https"]):
+            return False
+        if parsed.netloc not in allowed_domains:
+            return False
+        return not re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+    except TypeError:
+        print ("TypeError for ", parsed)
+        raise
+
+def extract_links(url, resp):
+    if resp.status != 200:
+        return []
+    
+    if not resp.raw_response or not resp.raw_response.content:
+        return []
+    
+    soup = BeautifulSoup(resp.raw_response.content, "lxml")
+    # links = [a.get("href") for a in soup.find_all("a")]
+
+    return [a.get("href") for a in soup.find_all("a")]
