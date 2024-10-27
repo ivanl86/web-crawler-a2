ed51b1a267139a7afd2599f508e6480b40515754
Ivan Leung <ivanl8866@gmail.com>
2024-10-27 14:12:04 -0700

    Fix regex in in_valid for checking date

--- scraper.py
+++ scraper.py
@@ -122,7 +122,7 @@ def is_valid(url):
                 return False
         
         # Find date in pattern yyyy-mm-dd
-        date_pattern = re.search(r"(\d{4}-\d{2})(-\d{2})?", parsed.query)
+        date_pattern = re.search(r"(\d{4}-\d{2}-\d{2})?", parsed.query)
 
         if date_pattern:
             date_str = date_pattern.group(1)
